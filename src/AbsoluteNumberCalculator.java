2d73b61492bb799afe377c2242574bc6bf9ac0d6
nmtuyen <nguyenmanhtuyen.haui@gmail.com>
2021-05-18 14:06:05 +0700

    update

--- src/AbsoluteNumberCalculator.java
+++ src/AbsoluteNumberCalculator.java
@@ -1,7 +1,7 @@
 public class AbsoluteNumberCalculator {
     public static int findAbsolute(int number){
-        if (number>0)
+        if (number>=0)
             return number;
-        return 0;
+        return -number;
     }
 }
--- test/AbsoluteNumberCalculatorTest.java
+++ test/AbsoluteNumberCalculatorTest.java
@@ -1,13 +1,25 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AbsoluteNumberCalculatorTest {
+
     @Test
-    void findAbsolute() {
+    void testFindAbsolute() {
         int number = 1;
         int expected = 1;
         int result = AbsoluteNumberCalculator.findAbsolute(number);
         assertEquals(result, expected);
     }
+
+    @Test
+    void testFindAbsolute2() {
+        int number = -1;
+        int expected = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(result, expected);
+
+    }
+
+
 }
